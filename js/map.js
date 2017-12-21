d99b81cff9bfbf01edb02922ffe781f43c47cfa3
ms-musmus <musmus001@yandex.ru>
2017-12-21 03:02:53 +0300

    #13 Личный проект: подробности

--- js/map.js
+++ js/map.js
@@ -44,6 +44,7 @@ var OFFER_TYPE_NAMES = {
 };
 var PIN_LEFT_OFFSET = 5;
 var PIN_TOP_OFFSET = 39;
+var ESC_KEYCODE = 27;
 
 // Переменные
 var pinTemplate = document.querySelector('template').content.querySelector('button.map__pin');
@@ -52,6 +53,9 @@ var cardTemplate = document.querySelector('template').content.querySelector('art
 var mapElement = document.querySelector('.map');
 var mapFilterElement = mapElement.querySelector('.map__filters-container');
 var similarAds = [];
+var mapPinMainElement = document.querySelector('.map__pin--main');
+var noticeFormElement = document.querySelector('form.notice__form');
+var noticeFieldSetElements = noticeFormElement.querySelectorAll('fieldset');
 
 // Вспомогательные функции
 // Возвращает случайное целое число между min и max (включительно)
@@ -69,18 +73,19 @@ var generateFeaturesArray = function () {
 };
 
 // Заполняет шаблон пина
-var renderPin = function (similarAd) {
+var renderPin = function (similarAd, id) {
   var pinElement = pinTemplate.cloneNode(true);
 
   pinElement.style.left = similarAd.location.x - PIN_LEFT_OFFSET + 'px';
   pinElement.style.top = similarAd.location.y - PIN_TOP_OFFSET + 'px';
   pinElement.querySelector('img').src = similarAd.author.avatar;
+  pinElement.dataset.customId = id;
 
   return pinElement;
 };
 
 // Заполняет шаблон объявления
-var renderCard = function (similarAd) {
+var renderCard = function (similarAd, id) {
   var cardElement = cardTemplate.cloneNode(true);
   var offerTitleElement = cardElement.querySelector('h3');
   var offerAddressElement = cardElement.querySelector('p small');
@@ -94,9 +99,7 @@ var renderCard = function (similarAd) {
 
   offerTitleElement.textContent = similarAd.offer.title;
   offerAddressElement.textContent = similarAd.offer.address;
-  offerPriceElement.textContent = similarAd.offer.price;
-  // Костыль для критерия Б28
-  offerPriceElement.innerHTML += '&#x20bd;/ночь';
+  offerPriceElement.textContent = similarAd.offer.price + '\u20BD/ночь';
   offerTypeElement.textContent = OFFER_TYPE_NAMES[similarAd.offer.type];
   offerSizeElement.textContent = similarAd.offer.rooms + ' для ' + similarAd.offer.guests + ' гостей';
   offerCheckElement.textContent = 'Заезд после ' + similarAd.offer.checkin + ', выезд до ' + similarAd.offer.checkout;
@@ -110,8 +113,84 @@ var renderCard = function (similarAd) {
   offerFeaturesElement.appendChild(fragment);
   offerDescriptionElement.textContent = similarAd.offer.description;
   offerAuthorAvatarElement.src = similarAd.author.avatar;
+  cardElement.dataset.customId = id;
 
   return cardElement;
+};
+
+
+// Отрисовывает пины
+var renderPinsFragment = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < similarAds.length; i++) {
+    var pin = renderPin(similarAds[i], i);
+    fragment.appendChild(pin);
+    pin.addEventListener('click', pinClickHandler);
+  }
+  if (mapPinsElement.children.length > 2) {
+    while (mapPinsElement.children[2]) {
+      mapPinsElement.removeChild(mapPinsElement.children[2]);
+    }
+  }
+  mapPinsElement.appendChild(fragment);
+};
+
+// Показывает карту
+var showMap = function () {
+  mapElement.classList.remove('map--faded');
+};
+
+// Делает форму активной
+var enableForm = function () {
+  noticeFormElement.classList.remove('notice__form--disabled');
+  for (i = 0; i < noticeFieldSetElements.length; i++) {
+    noticeFieldSetElements[i].disabled = false;
+  }
+};
+
+// Делает пин активным
+var activatePin = function (evt) {
+  deactivatePin();
+  evt.target.classList.add('map__pin--active');
+  showCard(evt.target);
+};
+
+// Делает пин неактивным
+var deactivatePin = function () {
+  var activePinElement = mapPinsElement.querySelector('.map__pin--active');
+  if (activePinElement) {
+    activePinElement.classList.remove('map__pin--active');
+  }
+};
+
+// Показывает объявление для текущего пина
+var showCard = function (pin) {
+  var activeCard = mapElement.querySelector('.map__card');
+  if (activeCard && activeCard.dataset.customId !== pin.dataset.customId) {
+    mapElement.removeChild(activeCard);
+    createCard(pin);
+  } else if (!activeCard) {
+    createCard(pin);
+  } else {
+    activeCard.classList.remove('hidden');
+  }
+  document.addEventListener('keydown', cardEscPressHandler);
+};
+
+// Создает новый элемент под объявление
+var createCard = function (pin) {
+  var card = renderCard(similarAds[pin.dataset.customId], pin.dataset.customId);
+  mapElement.insertBefore(card, mapFilterElement);
+  var cardCloseElement = card.querySelector('.popup__close');
+  cardCloseElement.addEventListener('click', cardCloseHandler);
+};
+
+// Закрывает окно с объявлением
+var closeCard = function () {
+  var activeCard = mapElement.querySelector('.map__card');
+  activeCard.classList.add('hidden');
+  deactivatePin();
+  document.removeEventListener('keydown', cardEscPressHandler);
 };
 
 for (var i = 0; i < 8; i++) {
@@ -141,12 +220,24 @@ for (var i = 0; i < 8; i++) {
   similarAds[i] = currentAd;
 }
 
-document.querySelector('.map').classList.remove('map--faded');
+var pinMouseUpHandler = function () {
+  showMap();
+  renderPinsFragment();
+  enableForm();
+};
 
-var fragment = document.createDocumentFragment();
-for (i = 0; i < similarAds.length; i++) {
-  fragment.appendChild(renderPin(similarAds[i]));
-}
-mapPinsElement.appendChild(fragment);
+var pinClickHandler = function (evt) {
+  activatePin(evt);
+};
 
-mapElement.insertBefore(renderCard(similarAds[0]), mapFilterElement);
+var cardCloseHandler = function () {
+  closeCard();
+};
+
+var cardEscPressHandler = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeCard();
+  }
+};
+
+mapPinMainElement.addEventListener('mouseup', pinMouseUpHandler);
